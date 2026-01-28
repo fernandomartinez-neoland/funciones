681a82976f23bfdad54622783da04a656fc4e466
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-01-28 12:13:12 +0100

    function #1

--- function.js
+++ function.js
@@ -1,1 +1,19 @@
-console.log("holi")
+console.log("holi linea 1")
+
+saludo()
+
+console.log("eso se ejecutara despues de la funcion de abajo")
+
+// la declaracion de una funcion esta estructurada de la siguiente manera y orden:
+//      1)palabra reservada function
+//      2)nombre de la funcion
+//      3)parentesis que recibiran (o no) los parametros
+//      4)seguido de llaves, que es donde ejecutaremos nuestras acciones, en este caos imprimir en consola
+
+
+function saludo(){
+    console.log("holi linea 6")
+}
+
+
+  
